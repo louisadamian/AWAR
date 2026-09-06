b71ea81b81219906aae67687c456d53667c70f41
Louis Adamian <louis@adamian.net>
2026-09-06 12:29:30 -0400

    wip play_by_play sql

--- awar/sql_writer.py
+++ awar/sql_writer.py
@@ -1,6 +1,51 @@
 import pandas as pd
 import sqlalchemy as sa
+from sqlalchemy import Column, Integer, Float
+from sqlalchemy.orm import Session
 import os
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+BASE_LABELS = {
+    0: "___",  # empty
+    1: "1__",  # runner on 1st
+    2: "_2_",  # runner on 2nd
+    3: "12_",  # 1st and 2nd
+    4: "__3",  # runner on 3rd
+    5: "1_3",
+    6: "_23",
+    7: "123",  # loaded
+}
+
+
+def __cell_col(outs: int, base_idx: int):
+    return f"re_{outs}out_{BASE_LABELS[base_idx]}"
+
+
+class RunExpectancy(Base):
+    __tablename__ = "run_expectancy"
+    year = Column(Integer, primary_key=True)
+
+
+for outs in range(3):
+    for base_idx in range(8):
+        setattr(
+            RunExpectancy,
+            __cell_col(outs, base_idx),
+            Column(__cell_col(outs, base_idx), Float),
+        )
+
+
+def write_run_expectancy(engine: sa.engine.Engine, year, re_grid):
+    values = {"year": int(year)}
+    for outs in range(3):
+        for base_idx in range(8):
+            values[__cell_col(outs, base_idx)] = float(re_grid[outs, base_idx])
+
+    with Session(engine) as s:
+        s.add(RunExpectancy(**values))
+        s.commit()
 
 
 def sql_write_play_by_play(engine: sa.engine.Engine, table: str, pbp: pd.DataFrame):
@@ -9,9 +54,11 @@ def sql_write_play_by_play(engine: sa.engine.Engine, table: str, pbp: pd.DataFra
     """
     pbp.to_sql(name=table, con=engine, index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import statsapi_adapter
     import mlbstatsapi
+
     url = sa.URL.create(
         drivername="postgresql",
         username=os.getenv("DB_USER"),
